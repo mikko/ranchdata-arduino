d0fecd7812a9ba8ac03f5e3e2eedb1f208fd4ac9
Mikko Pohja <mikko.pohja@futurice.com>
2016-12-04 23:42:33 +0200

    Sauna sensor using DS18B20 temperature sensor

---
+++ saunaSensor/saunaSensor.ino
@@ -0,0 +1,36 @@
+/* Arduino DS18B20 temp sensor tutorial
+   More info: http://www.ardumotive.com/how-to-use-the-ds18b20-temperature-sensor-en.html
+   Date: 19/6/2015 // www.ardumotive.com */
+
+
+//Include libraries
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LowPower.h>
+
+
+// Data wire is plugged into pin 8 on the Arduino
+#define ONE_WIRE_BUS 8
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  Serial.begin(9600); //Begin serial communication
+  Serial.println("Arduino Digital Temperature // Serial Monitor Version"); //Print a message
+  sensors.begin();
+}
+
+void loop(void)
+{ 
+  Serial.flush();
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  
+  // Send the command to get temperatures
+  sensors.requestTemperatures();  
+  Serial.print("Temperature is: ");
+  Serial.println(sensors.getTempCByIndex(0)); // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  //Update value every 1 sec.
+  // delay(1000);
+}
